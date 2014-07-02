ce7d4851a16d01da6d4e5fe0782c0a21ead7739f
Patrick Ruzand <pruzand@gmail.com>
2014-07-02 15:24:06 +0200

    improve tests

--- app/MyModule3.js
+++ app/MyModule3.js
@@ -1,16 +1,15 @@
 define([
 	"dcl/dcl",
-    "./load-css!./simple",
-    "./load-css!./simple11"
+    "./load-css!./simple3"
 ], function(dcl){
 		return dcl(null, {
 			constructor: function(){
 				console.log("MyModule ctor");
 				var elt = document.createElement("div");
 				if(elt.classList) {
-					elt.classList.add("mymodule");
+					elt.classList.add("mymodule3");
 				} else {
-					elt.className = "mymodule";
+					elt.className = "mymodule3";
 				}
 				elt.innerHTML = "<span>Hello from myModule!!!</span>";
 				document.body.appendChild(elt);
--- app/load-css.js
+++ app/load-css.js
@@ -5,277 +5,246 @@
 
 /*jslint browser:true, on:true, sub:true */
 
-define([], function(){
-	'use strict';
+define([
+	"requirejs-dplugins/has",
+	"dcl/dcl",
+	"module"
+], function (has, dcl, module) {
+	"use strict";
 
-/*
- * AMD css! plugin
- * This plugin will load and wait for css files.  This could be handy when
- * loading css files as part of a layer or as a way to apply a run-time theme.
- * Most browsers do not support the load event handler of the link element.
- * Therefore, we have to use other means to detect when a css file loads.
- * (The HTML5 spec states that the LINK element should have a load event, but
- * not even Chrome 8 or FF4b7 have it, yet.
- * http://www.w3.org/TR/html5/semantics.html#the-link-element)
- *
- * This plugin tries to use the load event and a universal work-around when
- * it is invoked the first time.  If the load event works, it is used on
- * every successive load.  Therefore, browsers that support the load event will
- * just work (i.e. no need for hacks!).  FYI, Feature-detecting the load
- * event is tricky since most browsers have a non-functional onload property.
- *
- * The universal work-around watches a stylesheet until its rules are
- * available (not null or undefined).  There are nuances, of course, between
- * the various browsers.  The isLinkReady function accounts for these.
- *
- * Note: it appears that all browsers load @import'ed stylesheets before
- * fully processing the rest of the importing stylesheet. Therefore, we
- * don't need to find and wait for any @import rules explicitly.
- *
- * Note #2: for Opera compatibility, stylesheets must have at least one rule.
- * AFAIK, there's no way to tell the difference between an empty sheet and
- * one that isn't finished loading in Opera (XD or same-domain).
- *
- * Options:
- *      !nowait - does not wait for the stylesheet to be parsed, just loads it
- *
- * Global configuration options:
- *
- * cssDeferLoad: Boolean. You can also instruct this plugin to not wait
- * for css resources. They'll get loaded asap, but other code won't wait
- * for them. This is just like using the !nowait option on every css file.
- *
- * cssWatchPeriod: if direct load-detection techniques fail, this option
- * determines the msec to wait between brute-force checks for rules. The
- * default is 50 msec.
- *
- * You may specify an alternate file extension:
- *      require('css!myproj/component.less') // --> myproj/component.less
- *      require('css!myproj/component.scss') // --> myproj/component.scss
- *
- * When using alternative file extensions, be sure to serve the files from
- * the server with the correct mime type (text/css) or some browsers won't
- * parse them, causing an error in the plugin.
- *
- * usage:
- *      require(['css!myproj/comp']); // load and wait for myproj/comp.css
- *      define(['css!some/folder/file'], {}); // wait for some/folder/file.css
- *      require(['css!myWidget!nowait']);
- *
- * Tested in:
- *      Firefox 1.5, 2.0, 3.0, 3.5, 3.6, and 4.0b6
- *      Safari 3.0.4, 3.2.1, 5.0
- *      Chrome 7 (8+ is partly b0rked)
- *      Opera 9.52, 10.63, and Opera 11.00
- *      IE 6, 7, and 8
- *      Netscape 7.2 (WTF? SRSLY!)
- * Does not work in Safari 2.x :(
- * In Chrome 8+, there's no way to wait for cross-domain (XD) stylesheets.
- * See comments in the code below.
- * TODO: figure out how to be forward-compatible when browsers support HTML5's
- *  load handler without breaking IE and Opera
-*/
+	/*
+	 * AMD css! plugin
+	 * This plugin will load and wait for css files.  This could be handy when
+	 * loading css files as part of a layer or as a way to apply a run-time theme.
+	 * Most browsers do not support the load event handler of the link element.
+	 * Therefore, we have to use other means to detect when a css file loads.
+	 * (The HTML5 spec states that the LINK element should have a load event, but
+	 * not even Chrome 8 or FF4b7 have it, yet.
+	 * http://www.w3.org/TR/html5/semantics.html#the-link-element)
+	 *
+	 * This plugin tries to use the load event and a universal work-around when
+	 * it is invoked the first time.  If the load event works, it is used on
+	 * every successive load.  Therefore, browsers that support the load event will
+	 * just work (i.e. no need for hacks!).  FYI, Feature-detecting the load
+	 * event is tricky since most browsers have a non-functional onload property.
+	 *
+	 * The universal work-around watches a stylesheet until its rules are
+	 * available (not null or undefined).  There are nuances, of course, between
+	 * the various browsers.  The isLinkReady function accounts for these.
+	 *
+	 * Note: it appears that all browsers load @import'ed stylesheets before
+	 * fully processing the rest of the importing stylesheet. Therefore, we
+	 * don't need to find and wait for any @import rules explicitly.
+	 *
+	 * Note #2: for Opera compatibility, stylesheets must have at least one rule.
+	 * AFAIK, there's no way to tell the difference between an empty sheet and
+	 * one that isn't finished loading in Opera (XD or same-domain).
+	 *
+	 * Global configuration options:
+	 *
+	 * cssWatchPeriod: if direct load-detection techniques fail, this option
+	 * determines the msec to wait between brute-force checks for rules. The
+	 * default is 50 msec.
+	 *
+	 * You may specify an alternate file extension:
+	 *      require('css!myproj/component.less') // --> myproj/component.less
+	 *      require('css!myproj/component.scss') // --> myproj/component.scss
+	 *
+	 * When using alternative file extensions, be sure to serve the files from
+	 * the server with the correct mime type (text/css) or some browsers won't
+	 * parse them, causing an error in the plugin.
+	 *
+	 * usage:
+	 *      require(['css!myproj/comp']); // load and wait for myproj/comp.css
+	 *      define(['css!some/folder/file'], {}); // wait for some/folder/file.css
+	 *
+	 * Tested in:
+	 *      Firefox 28+
+	 *      Safari 6+
+	 *      Chrome 33+
+	 *      IE 9+
+	 *      Android 4.x
+	 */
 
 
 	var
-		// compressibility shortcuts
-		onreadystatechange = 'onreadystatechange',
-		onload = 'onload',
-		createElement = 'createElement',
-		// failed is true if RequireJS threw an exception
+	// failed is true if RequireJS threw an exception
 		failed = false,
-		doc = document,
-		cache = typeof _css_cache == 'undefined' ? {} : _css_cache,
-		undef,
-		features = {
-			'event-link-onload': document.createElement('link').onload === null
-				// safari lies about the onload event
-				// PR: needed for Android Stock Browser...
-				&& !navigator.userAgent.match(/AppleWebKit/),
-			'dom-create-style-element': !document.createStyleSheet
-		},
-		// find the head element and set it to it's standard property if nec.
-		head = doc.head || (doc.head = doc.getElementsByTagName('head')[0]);
+		cache = {},
+		lastInsertedLink,
+		head = document && (document.head || document.getElementsByTagName("head")[0]);
 
-	function has (feature) {
-		return features[feature];
-	}
-	function createLink (doc, optHref) {
-		var link = doc[createElement]('link');
-		link.rel = 'stylesheet';
-		link.type = 'text/css';
-		if (optHref) {
-			link.href = optHref;
-		}
+	has.add("event-link-onload", function (global) {
+		return global.document && global.document.createElement("link").onload === null
+			// safari lies about the onload event
+			// PR: needed for Android Stock Browser...
+			//&& !navigator.userAgent.match(/AppleWebKit/);
+	});
+
+	console.log("has(event-link-onload): " + has("event-link-onload"));
+
+	function createLink() {
+		var link = document.createElement("link");
+		link.rel = "stylesheet";
+		link.type = "text/css";
 		return link;
 	}
-	function nameWithExt (name, defaultExt) {
-		return name.lastIndexOf('.') <= name.lastIndexOf('/') ?
-			name + '.' + defaultExt : name;
-	}
-	function parseSuffixes (name) {
-		// creates a dual-structure: both an array and a hashmap
-		// suffixes[0] is the actual name
-		var parts = name.split('!'),
-			suf, i = 1, pair;
-		while ((suf = parts[i++])) { // double-parens to avoid jslint griping
-			pair = suf.split('=', 2);
-			parts[pair[0]] = pair.length == 2 ? pair[1] : true;
-		}
-		return parts;
-	}
-		
 
-	if(!has('bundled-css')){ // if all the CSS is bundled, we don't need to the loader code
-		var loadDetector = function(params, cb){
-			// failure detection
-			// we need to watch for onError when using RequireJS so we can shut off
-			// our setTimeouts when it encounters an error.
-			if (require.onError) {
-				require.onError = (function (orig) {
-					return function () {
-						failed = true;
-						orig.apply(this, arguments);
-					};
-				})(require.onError);
-			}
-		
-			/***** load-detection functions *****/
-		
-			function loadHandler (params, cb) {
-				// We're using 'readystatechange' because IE and Opera happily support both
-				var link = params.link;
-				link[onreadystatechange] = link[onload] = function () {
-					if (!link.readyState || link.readyState == 'complete') {
-						features['event-link-onload'] = true;
-						cleanup(params);
-						cb();
-					}
+	function nameWithExt(name, defaultExt) {
+		return (/\.[^/]*$/.test(name)) ? name : name + "." + defaultExt;
+	}
+
+	var loadDetector = function (params, cb) {
+		// failure detection
+		// we need to watch for onError when using RequireJS so we can shut off
+		// our setTimeouts when it encounters an error.
+		if (require.onError) {
+			require.onError = (function (orig) {
+				return function () {
+					failed = true;
+					orig.apply(this, arguments);
 				};
-			}
-		
-		
-			function isLinkReady (link) {
-				// based on http://www.yearofmoo.com/2011/03/cross-browser-stylesheet-preloading.html
-				// Therefore, we need
-				// to continually test beta browsers until they all support the LINK load
-				// event like IE and Opera.
-				// webkit's and IE's sheet is null until the sheet is loaded
-				var sheet = link.sheet || link.styleSheet;
-				if(sheet){
-					var styleSheets = document.styleSheets;
-					for(var i = styleSheets.length; i > 0; i--){
-						if(styleSheets[i-1] == sheet){
-							return true;
-						}
+			})(require.onError);
+		}
+
+		/***** load-detection functions *****/
+
+		function loadHandler(params, cb) {
+			// We're using "readystatechange" because IE and Opera happily support both
+			var link = params.link;
+			link.onreadystatechange = link.onload = function () {
+				if (!link.readyState || link.readyState === "complete") {
+					console.log("##### event-link-onload is really true !!!");
+					has.add("event-link-onload", true, true, true);
+					cleanup(params);
+					cb(params);
+				}
+			};
+		}
+
+		// alternative path for browser with broken link-onload
+
+		function isLinkReady(link) {
+			// based on http://www.yearofmoo.com/2011/03/cross-browser-stylesheet-preloading.html
+			// Therefore, we need
+			// to continually test beta browsers until they all support the LINK load
+			// event like IE and Opera.
+			// webkit's and IE's sheet is null until the sheet is loaded
+			var sheet = link.sheet || link.styleSheet;
+			if (sheet) {
+				var styleSheets = document.styleSheets;
+				for (var i = styleSheets.length; i > 0; i--) {
+					if (styleSheets[i - 1] === sheet) {
+						return true;
 					}
 				}
 			}
-		
-			function ssWatcher (params, cb) {
-				// watches a stylesheet for loading signs.
-				if (isLinkReady(params.link)) {
-					cleanup(params);
-					cb();
-				}
-				else if (!failed) {
-					setTimeout(function () { ssWatcher(params, cb); }, params.wait);
-				}
-			}
-		
-			function cleanup (params) {
-				var link = params.link;
-				link[onreadystatechange] = link[onload] = null;
-			}
-			// It would be nice to use onload everywhere, but the onload handler
-			// only works in IE and Opera.
-			// Detecting it cross-browser is completely impossible, too, since
-			// THE BROWSERS ARE LIARS! DON'T TELL ME YOU HAVE AN ONLOAD PROPERTY
-			// IF IT DOESN'T DO ANYTHING!
-			var loaded;
-			function cbOnce () {
-				if (!loaded) {
-					loaded = true;
-					cb();
-				}
-			}
-			loadHandler(params, cbOnce);
-			if (!has('event-link-onload')) {
-				ssWatcher(params, cbOnce);
-			}
-		
-		};
-	}
-	function insertCss(css){
-		if(has('dom-create-style-element')){
-			// we can use standard <style> element creation
-			styleSheet = document.createElement('style');
-			styleSheet.setAttribute('type', 'text/css');
-			styleSheet.appendChild(document.createTextNode(css));
-			head.insertBefore(styleSheet, head.firstChild);
-			return styleSheet;
 		}
-		else{
-			var styleSheet = document.createStyleSheet();
-			styleSheet.cssText = css;
-			return styleSheet.owningElement;
+
+		function ssWatcher(params, cb) {
+			// watches a stylesheet for loading signs.
+			if (isLinkReady(params.link)) {
+				cleanup(params);
+				cb(params);
+			}
+			else if (!failed) {
+				setTimeout(function () {
+					ssWatcher(params, cb);
+				}, params.wait);
+			}
 		}
-	}
+
+		function cleanup(params) {
+			var link = params.link;
+			link.onreadystatechange = link.onload = null;
+		}
+
+		// It would be nice to use onload everywhere, but the onload handler
+		// only works in IE and Opera.
+		// Detecting it cross-browser is completely impossible, too, since
+		// THE BROWSERS ARE LIARS! DON'T TELL ME YOU HAVE AN ONLOAD PROPERTY
+		// IF IT DOESN'T DO ANYTHING!
+		var loaded;
+
+		function cbOnce() {
+			if (!loaded) {
+				loaded = true;
+				cb(params);
+			}
+		}
+
+		loadHandler(params, cbOnce);
+		if (!has("event-link-onload")) {
+			console.log("### try ssWatcher path...");
+			ssWatcher(params, cbOnce);
+		}
+	};
+
 	/***** finally! the actual plugin *****/
 	return {
 
+		load: function (resourceDef, require, callback) {
+			var resources = resourceDef.split(","),
+				config = module.config(),
+				loadingCount = resources.length,
 
-
-			load: function (resourceDef, require, callback, config) {
-				var resources = resourceDef.split(','),
-					loadingCount = resources.length,
-
-				// all detector functions must ensure that this function only gets
-				// called once per stylesheet!
-					loaded = function () {
-						// load/error handler may have executed before stylesheet is
-						// fully parsed / processed in Opera, so use setTimeout.
-						// Opera will process before the it next enters the event loop
-						// (so 0 msec is enough time).
-						if (--loadingCount == 0) {
-							// TODO: move this setTimeout to loadHandler
-							callback(link.sheet || link.styleSheet);
-							// TODO: Is this need for Opera?
-							//setTimeout(onCssLoaded,0);
-						}
-					};
-
-				// after will become truthy once the loop executes a second time
-				for (var i = 0, after; i < resources.length; i++, after = url) {
-					resourceDef = resources[i];
-					var cached = cache[resourceDef];
-					if (cached) {
-						link = insertCss(cached);
-						return loaded();
+			// all detector functions must ensure that this function only gets
+			// called once per stylesheet!
+				loaded = function (params) {
+					// load/error handler may have executed before stylesheet is
+					// fully parsed / processed in Opera, so use setTimeout.
+					// Opera will process before the it next enters the event loop
+					// (so 0 msec is enough time).
+					var cached = cache[params.url];
+					cached.s = "loaded";
+					var cbs = cached.cbs;
+					delete cached.cbs;
+					if (cbs) {
+						cbs.forEach(function (f) { f(); });
 					}
-					var
-					// TODO: this is a bit weird: find a better way to extract name?
-						opts = parseSuffixes(resourceDef),
-						name = opts.shift(),
-						url = nameWithExt(require['toUrl'](name), 'css'),
-						link = createLink(doc),
-						nowait = 'nowait' in opts ? opts.nowait != 'false' : !!(config && config.cssDeferLoad),
-						params = {
-							link: link,
-							url: url,
-							wait: config && config.cssWatchPeriod || 25
-						};
-					// hook up load detector(s)
-					loadDetector(params, loaded);
-					if (nowait) {
-						callback(link);
+					// if all stylesheets have been loaded, then call the plugin callback
+					if (--loadingCount === 0) {
+						callback(link.sheet || link.styleSheet);
 					}
+				};
 
-					// go!
-					link.href = url;
-
-					head.appendChild(link);
+			// after will become truthy once the loop executes a second time
+			for (var i = 0, after; i < resources.length; i++, after = url) {
+				resourceDef = resources[i];
+				var
+					name = resourceDef,
+					url = nameWithExt(require["toUrl"](name), "css"),
+					link = createLink(),
+				// TODO PR: should we still support these options ?
+					params = {
+						link: link,
+						url: url,
+						wait: config && config.cssWatchPeriod || 25
+					},
+					cached = cache[url];
+				if (cached) {
+					switch (cached.s) {
+					case "loaded":
+						loaded(params);
+						console.log(url + " has already been loaded");
+						continue;
+					case "injected":
+						// if the link has been injected in a previous load() call but not yet loaded,
+						// we register the loaded callback of this module to get called when the injected css will be
+						// loaded, and process the next resourceDef, if any.
+						var f = loaded.bind(this, params);
+						cached.cbs ? cached.cbs.push[f] : (cached.cbs = [f]);
+						continue;
+					}
 				}
+				cache[params.url] = {s: "injected"};
+				// hook up load detector(s)
+				loadDetector(params, loaded);
+				// go!
+				link.href = url;
+				head.insertBefore(link, lastInsertedLink ? lastInsertedLink.nextSibling : head.firstChild);
+				lastInsertedLink = link;
 			}
+		}
 	};
 });
